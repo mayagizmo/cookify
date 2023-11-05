da0fcf0504a155e1c2357431912c4264d686e228
Maike <brauer-maike@web.de>
2023-11-05 11:43:25 +0100

    Implement header

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,3 +1,18 @@
+import { LuShoppingCart } from "react-icons/lu";
+
+import styles from "./Header.module.scss";
+
 export function Header() {
-  return <h1>Shopping List</h1>;
+  return (
+    <header className={styles.header}>
+      <h1>Shopping List</h1>
+
+      <span className={styles.icon}>
+        <LuShoppingCart />
+      </span>
+      <small className={styles.headerSubtitle}>
+        Shopping ingredients for tasty food
+      </small>
+    </header>
+  );
 }
