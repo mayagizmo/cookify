21af59b3b51b515a138953c9a43e4301619f3b00
Maike <brauer-maike@web.de>
2023-11-05 10:49:21 +0100

    Add container

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import { ShoppingList } from "./ShoppingList/ShoppingList.tsx";
 import { Footer } from "./layouts/Footer.tsx";
 import { Header } from "./layouts/Header.tsx";
+import "./global.scss";
 
 export function App() {
   return (
-    <>
+    <main className="container">
       <Header />
 
       <ShoppingList />
 
       <Footer />
-    </>
+    </main>
   );
 }
