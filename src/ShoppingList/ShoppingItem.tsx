f8fa8c71e3d9e0b2cd7c511b7f5805a92b999efa
Maike <brauer-maike@web.de>
2023-11-06 20:05:29 +0100

    Add label to make the whole item clickable

--- src/ShoppingList/ShoppingItem.tsx
+++ src/ShoppingList/ShoppingItem.tsx
@@ -4,14 +4,16 @@ import styles from "./ShoppingItem.module.scss";
 
 export function ShoppingItem() {
   return (
-    <div className={styles.entireItem}>
-      <span className={styles.checkBox}>
-        <input type="checkbox" />
-      </span>
-      <span className={styles.itemName}>Mushroomies</span>
-      <span className={styles.trashCan}>
-        <BsFillTrash3Fill />
-      </span>
-    </div>
+    <label>
+      <div className={styles.entireItem}>
+        <span className={styles.checkBox}>
+          <input type="checkbox" />
+        </span>
+        <span className={styles.itemName}>Mushroomies</span>
+        <span className={styles.trashCan}>
+          <BsFillTrash3Fill />
+        </span>
+      </div>
+    </label>
   );
 }
