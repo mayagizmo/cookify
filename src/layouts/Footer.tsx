63545b5eee0948e905bb12762b31d55b947d0e99
Maike <brauer-maike@web.de>
2023-11-06 20:06:18 +0100

    Add new color scheme/design

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -3,7 +3,7 @@ import styles from "./Footer.module.scss";
 export function Footer() {
   return (
     <footer className={styles.footer}>
-      &copy; 2023 Made with ❤️ in Berlin
+      &copy; 2023 Made with <span className={styles.heart}>❤</span> in Berlin
     </footer>
   );
 }
