433e36a2333066f528dec4cfaab9524d83e12a7d
mayagizmo <brauer-maike@web.de>
2024-02-11 14:37:11 +0100

    Add header

---
+++ src/Header/Header.tsx
@@ -0,0 +1,24 @@
+import cx from "classnames";
+import { BsJournalPlus } from "react-icons/bs";
+import { GiFoodTruck } from "react-icons/gi";
+
+import styles from "./Header.module.css";
+
+export function Header() {
+  return (
+    <div className={cx(styles.headerBar, "container")}>
+      <a>
+        <div className={styles.logo}>
+          <span className={styles.foodTruckIcon}>
+            <GiFoodTruck />
+          </span>
+          <span className={styles.name}>Cookify</span>
+        </div>
+      </a>
+
+      <a href="#" className={styles.addRecipeLink}>
+        <BsJournalPlus />
+      </a>
+    </div>
+  );
+}
