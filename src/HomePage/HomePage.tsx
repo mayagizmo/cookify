08129d08c51261c076a68d2f96831e8ffe2d3ef5
mayagizmo <brauer-maike@web.de>
2024-04-21 18:28:49 +0200

    Add Loading Message while fetching

--- src/HomePage/HomePage.tsx
+++ src/HomePage/HomePage.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { FaSpinner } from "react-icons/fa";
 
 import { RecipeCard, RecipeProps } from "../RecipeCard/RecipeCard.tsx";
 
@@ -9,9 +10,12 @@ interface APIResponse {
 export function HomePage() {
   const [recipes, setRecipes] = useState<Array<RecipeProps>>([]);
   const [isError, setIsError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
   useEffect(() => {
     async function fetchRecipes() {
       try {
+        setIsLoading(true);
         const response = await fetch(
           "https://cookify-cloudflare.windesign.workers.dev/recipes",
         );
@@ -30,6 +34,8 @@ export function HomePage() {
         console.error("my fetch error is=", error);
         // Output e.g.: "Fetch Error: 404, Not found"
         setIsError(true);
+      } finally {
+        setIsLoading(false);
       }
     }
     void fetchRecipes();
@@ -54,6 +60,12 @@ export function HomePage() {
       {isError && (
         <div className="alert alert-error">
           There seems to be a problem. Sorry! 😢
+        </div>
+      )}
+      {!isLoading && (
+        <div className="alert alert-info">
+          <FaSpinner className="spinner" /> Loading fingerlicking good recipes
+          for you. Just one more tummy growl away.
         </div>
       )}
       <br />
