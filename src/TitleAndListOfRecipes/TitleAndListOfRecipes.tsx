ec433f30aa55c4c39c49b54a9acae076846c26f3
mayagizmo <brauer-maike@web.de>
2024-06-08 14:08:55 +0200

    Using new RecipeCardProps

--- src/TitleAndListOfRecipes/TitleAndListOfRecipes.tsx
+++ src/TitleAndListOfRecipes/TitleAndListOfRecipes.tsx
@@ -1,19 +1,23 @@
 import { Link } from "@tanstack/react-router";
 
-import { RecipeCard, RecipeProps } from "../RecipeCard/RecipeCard.tsx";
+import { RecipeCard, RecipeCardProps } from "../RecipeCard/RecipeCard.tsx";
 
 export interface TitleAndListOfRecipesProps {
   title: string;
-  recipes: Array<RecipeProps>;
+  recipes: Array<RecipeCardProps>;
+  link: string;
 }
 
 export function TitleAndListOfRecipes({
   title,
   recipes,
+  link,
 }: TitleAndListOfRecipesProps) {
   return (
     <>
-      <h2 className="text-2xl font-bold m-4 text-primary">{title}</h2>
+      <h2 className="text-2xl font-bold m-4 text-primary">
+        <Link to={link}>{title}</Link>
+      </h2>
       <section className="grid gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         {recipes.slice(0, 4).map((recipe) => (
           <Link
@@ -24,10 +28,11 @@ export function TitleAndListOfRecipes({
             key={recipe.title}
           >
             <RecipeCard
-              key={recipe.title}
+              key={recipe.id}
               title={recipe.title}
               prepTime={recipe.prepTime}
               cookingTime={recipe.cookingTime}
+              id={recipe.id}
             />
           </Link>
         ))}
