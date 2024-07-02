699eb96112f892095c46550e2c68563cbbf448bc
mayagizmo <brauer-maike@web.de>
2024-07-02 19:21:17 +0200

    WIP

--- src/AddRecipePage/AddRecipePage.tsx
+++ src/AddRecipePage/AddRecipePage.tsx
@@ -1,4 +1,4 @@
-import { useId, useState } from "react";
+import { FormEvent, useId, useState } from "react";
 import { FaSpinner } from "react-icons/fa";
 
 import { API_BASE } from "../constants.ts";
@@ -10,8 +10,10 @@ interface RecipePayload {
   instructions: string;
   ingredients: Array<string>;
   images?: string;
-  source?: string;
+  source?: Array<string>;
 }
+
+type Sources = Record<string, string>;
 
 export function AddRecipePage() {
   const [recipeTitle, setRecipeTitle] = useState("");
@@ -19,6 +21,9 @@ export function AddRecipePage() {
   const [prepTime, setPrepTime] = useState("");
   const [cookTime, setCookTime] = useState("");
   const [instructions, setInstructions] = useState("");
+  const [newSource, setNewSource] = useState("");
+  //const [sourcesList, setSourcesList] = useState<Array<string>>([]);
+  const [sources, setSources] = useState<Sources>({});
   const [isSuccess, setIsSuccess] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
@@ -27,6 +32,7 @@ export function AddRecipePage() {
   const prepTimeId = useId();
   const cookTimeId = useId();
   const instructionsId = useId();
+  const newSourceId = useId();
 
   async function postNewRecipe(data: RecipePayload) {
     setIsLoading(true);
@@ -50,6 +56,7 @@ export function AddRecipePage() {
       setIsLoading(false);
     }
   }
+
   function buildPayload(): RecipePayload {
     return {
       title: recipeTitle,
@@ -66,15 +73,39 @@ export function AddRecipePage() {
     setInstructions("");
     setPrepTime("");
     setCookTime("");
+    setSources({});
   }
 
-  function handleAddRecipe(e: React.FormEvent) {
+  async function handleAddRecipe(e: FormEvent) {
     e.preventDefault();
 
     setIsSuccess(false);
     setIsError(false);
 
-    postNewRecipe(buildPayload());
+    await postNewRecipe(buildPayload());
+  }
+
+  function handleAddReference(e: FormEvent) {
+    e.preventDefault();
+
+    const key = Math.random().toString();
+
+    setSources({ ...sources, [key]: newSource });
+
+    setNewSource("");
+  }
+
+  function updateSource(key: string, value: string) {
+    setSources({
+      ...sources,
+      [key]: value,
+    });
+  }
+
+  function deleteSource(key: string) {
+    const clonedSources = { ...sources };
+    delete clonedSources[key];
+    setSources(clonedSources);
   }
 
   return (
@@ -159,6 +190,45 @@ export function AddRecipePage() {
           min={1}
         />
       </div>
+      <div className="flex items-center gap-2 mb-2">
+        <label htmlFor={newSourceId} className="w-32">
+          References
+        </label>
+        <input
+          className="input input-bordered flex-grow"
+          name="source"
+          value={newSource}
+          id={newSourceId}
+          type="text"
+          onChange={(e) => setNewSource(e.target.value)}
+        />
+        <button className="btn" onClick={handleAddReference}>
+          Add
+        </button>
+      </div>
+      {Object.keys(sources).length > 0 && (
+        <div>
+          {Object.keys(sources).map((key) => (
+            <div key={key}>
+              <input
+                className="input input-bordered flex-"
+                name={sources[key]}
+                id={sources[key]}
+                value={sources[key]}
+                type="text"
+                onChange={(e) => updateSource(key, e.target.value)}
+              />
+              <button
+                className="btn-s"
+                type="button"
+                onClick={() => deleteSource(key)}
+              >
+                ❌
+              </button>
+            </div>
+          ))}
+        </div>
+      )}
 
       <div className="text-center mb-2">
         <button className="btn btn-primary" type="submit">
